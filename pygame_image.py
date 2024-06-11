9e05cc57be62ff228e2783ea1a00051a626b396d
C0B23126 <c0b2312652@edu.teu.ac.jp>
2024-06-11 16:04:32 +0900

    練習4：こうかとんの描画

--- pygame_image.py
+++ pygame_image.py
@@ -18,6 +18,9 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        kk_rct = kk_img.get_rect()  #こうかとんrectの抽出
+        kk_rct.center = 300, 200
+        screen.blit(kk_img, kk_rct) #kk_imgをkk_rctの設定に従って貼り付け 
         pg.display.update()
         tmr += 1        
         clock.tick(10)

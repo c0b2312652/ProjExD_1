a88fa6332a6882a828a9196c73879057e6a46092
C0B23126 <c0b2312652@edu.teu.ac.jp>
2024-06-11 16:37:53 +0900

    練習7：背景画像のワープ対策

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -17,8 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        kk_img_x = tmr % 800
+        kk_img_x = tmr % 3200
         screen.blit(bg_img, [-kk_img_x, 0])
+        screen.blit(bg_img2, [-kk_img_x + 1600, 0])
+        screen.blit(bg_img, [-kk_img_x + 3200, 0])
+        screen.blit(bg_img2, [-kk_img_x + 4800, 0])
         kk_rct = kk_img.get_rect()  #こうかとんrectの抽出
         kk_rct.center = 300, 200
         screen.blit(kk_img, kk_rct) #kk_imgをkk_rctの設定に従って貼り付け 

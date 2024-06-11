8843ce51b8a8b812a48f89e0d7f39e2c6870bb0a
C0B23126 <c0b2312652@edu.teu.ac.jp>
2024-06-11 16:52:05 +0900

    練習8：キーに応じたこうかとんの動作

--- pygame_image.py
+++ pygame_image.py
@@ -13,7 +13,11 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  #こうかとんrectの抽出
+    kk_rct.center = 300, 200
+    
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -23,8 +27,16 @@ def main():
         screen.blit(bg_img2, [-kk_img_x + 1600, 0])
         screen.blit(bg_img, [-kk_img_x + 3200, 0])
         screen.blit(bg_img2, [-kk_img_x + 4800, 0])
-        kk_rct = kk_img.get_rect()  #こうかとんrectの抽出
-        kk_rct.center = 300, 200
+
+        key_lst = pg.key.get_pressed()  #全てのキーの押下状態を取得
+        if key_lst[pg.K_UP]:    #上キーを押したら
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  #下キーを押したら
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:    #左キーを押したら
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  #右キーを押したら
+            kk_rct.move_ip(1, 0)
         screen.blit(kk_img, kk_rct) #kk_imgをkk_rctの設定に従って貼り付け 
         pg.display.update()
         tmr += 1        
